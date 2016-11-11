ef30969eda0ffea21d27156c4cedf97c003e2fd8
philoniare <philoniare@gmail.com>
2016-11-11 11:37:10 -0500

    feat: removed boilerplate code

--- model/article.py
+++
@@ -1,5 +0,0 @@
-from google.appengine.ext import db
-class Article(db.Model):
-    title = db.StringProperty(required = True)
-    text = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
